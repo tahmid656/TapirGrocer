c05679fedf4085a223a152deebf9f16de3af2589
Tahmid Rahman <tahmidrahman.656@gmail.com>
2020-11-30 01:12:57 +0800

    Final Commit

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -1,7 +1,7 @@
 class LinkController < ApplicationController
   require 'rqrcode'
   def index
-    @qr = RQRCode::QRCode.new("http://localhost:3000/taprigrocer/app/views/entries/new")
+    @qr = RQRCode::QRCode.new("http://localhost:3000/entries/new")
 # With default options specified explicitly
 
   end 
